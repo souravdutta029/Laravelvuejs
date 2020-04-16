c6154da95b13fd3769709be3f81c0f47d1ac2f07
“Sourav <duttasourav029@gmail>
2020-04-16 12:52:25 +0530

    User Component

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,7 +19,8 @@ Vue.use(VueRouter)
 // Routes
 const routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default},
-    { path: '/profile', component: require('./components/Profile.vue').default}
+    { path: '/users', component: require('./components/Users.vue').default},
+    { path: '/profile', component: require('./components/Profile.vue').default},
   ]
 
 
