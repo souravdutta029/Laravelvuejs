687895aa291470986d60f749356203e0bf1f6f29
“Sourav <duttasourav029@gmail>
2020-04-16 12:07:55 +0530

    making logout link work

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -108,10 +108,17 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-power-off"></i>
-              <p>Logout</p>
+            <a class="nav-link" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                              document.getElementById('logout-form').submit();">
+                <i class="nav-icon fas fa-power-off"></i>
+                <p>{{ __('Logout') }}</p>
             </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+
           </li>
           
         </ul>
